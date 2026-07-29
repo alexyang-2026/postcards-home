844827496f237192b6b519d1dc0886ee05788e26
Alex Yang <zixuan.yang2018@gmail.com>
2026-07-29 00:03:25 -0400

    chore: add collectible assets and project structure

---
+++ js/collectibles.js
@@ -0,0 +1,30 @@
+const collectibles = {
+    fireworks: {
+        id: "fireworks",
+        name: "Fireworks",
+        type: "postcard_background",
+        image: "assets/collectibles/backgrounds/fireworks.png",
+        rarity: "Rare",
+        description: "A beautiful fireworks background for your postcard!"
+    },
+
+    sakura: {
+        id: "sakura",
+        name: "Sakura",
+        type: "postcard_background",
+        image: "assets/collectibles/backgrounds/sakura.png",
+        rarity: "Epic",
+        description: "A gentle spring evening with cherry blossoms."
+    },
+
+    starry_night: {
+        id: "starrynight",
+        name: "Starry Night",
+        type: "postcard_background",
+        image: "assets/collectibles/backgrounds/starry-night.png",
+        rarity: "Legendary",
+        description: "A peaceful starry night sky."
+    }
+
+    
+};
